db65bc3faf56c7f163dc362f5ae5e902f045abb3
pmccullough060 <pmccullough060@gmail.com>
2020-09-20 22:37:51 +0100

    first commit

---
+++ JavaScriptFiles/OrbitControl.js
@@ -0,0 +1,87 @@
+"use strict";
+
+class rotationControls{
+
+    constructor(inputScene, inputCamera, inputRenderer, inputDocument){
+        this.scene = inputScene;
+        this.camera = inputCamera;
+        this.renderer = inputRenderer;
+        this.document = inputDocument;
+    }
+
+    add(){
+        //need to read ES6 docs about classes to understand this fully
+        //objects passed by ref in javascript??
+
+        var camera = this.camera;
+        var renderer = this.renderer;
+        var scene = this.scene;
+
+        var onClickStartX = null;
+        var onClickStartY = null;
+
+        function onMouseDown( event ){
+
+            onClickStartX = event.clientX;
+            onClickStartY = event.clientY;
+        };
+
+        function onMouseMove( event ){
+            if(onClickStartX === null || onClickStartY === null){
+                return;
+            }
+            else{
+                CalculateCameraPositions(event.clientX - onClickStartX,  event.clientY - onClickStartY);
+            };
+        };
+
+        function onMouseUp( event ){
+
+            CalculateCameraPositions(event.clientX - onClickStartX, event.clientY - onClickStartY);
+
+            onClickStartX = null;
+            onClickStartY = null;
+        };
+
+        document.addEventListener("mousedown", onMouseDown);
+        document.addEventListener("mouseup", onMouseUp);
+        document.addEventListener("mousemove", onMouseMove);
+
+        function CalculateCameraPositions(deltaX, deltaY){
+            
+            var sensitivity = 5000;
+            var radianPerPixel = (Math.PI / sensitivity);
+            var deltaPhi = radianPerPixel * deltaX;
+            var deltaTheta = radianPerPixel * deltaY;
+            var centre = new THREE.Vector3();
+            var cameraPosition = camera.position.sub(centre);
+            var radius = cameraPosition.length();
+            var theta = Math.acos(cameraPosition.z / radius);
+            var phi = Math.atan2(cameraPosition.y, cameraPosition.x);
+
+            theta = Math.min(Math.max(theta - deltaTheta, 0), Math.PI);
+
+            phi -= deltaPhi;
+
+            cameraPosition.x = radius * Math.sin(theta) * Math.cos(phi);
+            cameraPosition.y = radius * Math.sin(theta) * Math.sin(phi);
+            cameraPosition.z = radius * Math.cos(theta);
+
+            camera.position.add(centre);
+            camera.lookAt(centre);
+
+            Refresh();
+        }
+
+        var frameId = 0;
+
+        function Refresh(){
+            cancelAnimationFrame(frameId);
+            frameId = requestAnimationFrame(renderFrame)
+        };
+
+        function renderFrame(){
+            renderer.render(scene, camera);
+        };
+    };
+};
---
+++ JavaScriptFiles/index.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var scene1, camera1, renderer1;
+
+initScene();
+
+function initScene(){
+
+    scene1 = new THREE.Scene();
+    
+    camera1 = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+    camera1.up = new THREE.Vector3(0, 0, 1);
+    camera1.position.x = 5;
+
+    var centre = new THREE.Vector3();
+
+    camera1.lookAt(centre);
+    
+    renderer1 = new THREE.WebGLRenderer({antialias: true});
+    renderer1.setSize(window.innerWidth, window.innerHeight);
+    renderer1.setClearColor("#e5e5e5");
+    
+    document.body.appendChild(renderer1.domElement);
+};
+
+var control = new rotationControls(scene1, camera1, renderer1);
+
+//testing
+console.log(control.hasOwnProperty('scene'));
+console.log(control.hasOwnProperty('camera'));
+console.log(control.hasOwnProperty('renderer'));
+//testing 
+
+control.add();
+
+window.addEventListener('resize', () => {
+    renderer1.setSize(window.innerWidth, window.innerHeight);
+    camera1.aspect = window.innerWidth / window.innerHeight;
+
+    camera1.updateProjectionMatrix();
+});
+
+
+var geometry = new THREE.BoxGeometry(1,1,1);
+var material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
+var mesh = new THREE.Mesh(geometry, material);
+
+scene1.add(mesh);
+
+var color = new THREE.Color(0.2, 0.2, 0.2);
+var ambient = new THREE.AmbientLight(color.getHex());
+scene1.add(ambient);
+
+var light = new THREE.PointLight(0xFFFFFF);
+light.position.x = 5;
+light.position.y = 5;
+scene1.add(light);
+
+var axisHelper = new THREE.AxesHelper(5);
+scene1.add(axisHelper);
+
+renderer1.render(scene1, camera1);
